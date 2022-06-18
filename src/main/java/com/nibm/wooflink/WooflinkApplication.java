b5748448c2fcb6372a0b390094cc28b00271759a
dumidu1998 <dumiduraj@gmail.com>
2022-06-19 01:59:15 +0530

    init

---
+++ src/main/java/com/nibm/wooflink/WooflinkApplication.java
@@ -0,0 +1,13 @@
+package com.nibm.wooflink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WooflinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WooflinkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nibm/wooflink/WooflinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nibm.wooflink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WooflinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
