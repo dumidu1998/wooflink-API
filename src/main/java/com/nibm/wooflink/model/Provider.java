a55f4cf6d073b298c98e7901e74999e05066ee3d
dumidu1998 <dumiduraj@gmail.com>
2022-06-20 04:26:28 +0530

    anno added

--- src/main/java/com/nibm/wooflink/model/Provider.java
+++ src/main/java/com/nibm/wooflink/model/Provider.java
@@ -1,4 +1,16 @@
 package com.nibm.wooflink.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Setter
+@Getter
 public class Provider {
 }
