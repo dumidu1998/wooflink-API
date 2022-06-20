abdb7e963abd3a072cc660d89896f14a26fe5095
dumidu1998 <dumiduraj@gmail.com>
2022-06-20 12:01:58 +0530

    controllers created

---
+++ src/main/java/com/nibm/wooflink/controller/InvoiceController.java
@@ -0,0 +1,9 @@
+package com.nibm.wooflink.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/invoice")
+@RestController
+public class InvoiceController {
+}
